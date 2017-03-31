03662b971caafa928b4481ce7e20210235726afa
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-03-31 13:53:27 -0400

    Add head field to console_bridge

--- console_bridge.rb
+++ console_bridge.rb
@@ -3,6 +3,7 @@ class ConsoleBridge < Formula
   homepage "http://wiki.ros.org/console_bridge"
   url "https://github.com/ros/console_bridge/archive/0.3.2.tar.gz"
   sha256 "fd12e48c672cb9c5d516d90429c4a7ad605859583fc23d98258c3fa7a12d89f4"
+  head "https://github.com/ros/console_bridge", :branch => "master"
 
   depends_on "cmake" => :build
 
