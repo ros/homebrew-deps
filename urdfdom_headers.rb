133c6997e48fd680b22c7da8363743abadc8ac6a
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-03-31 13:52:54 -0400

    Add head field to urdfdom_headers

--- urdfdom_headers.rb
+++ urdfdom_headers.rb
@@ -3,6 +3,7 @@ class UrdfdomHeaders < Formula
   homepage "http://wiki.ros.org/urdfdom_headers"
   url "https://github.com/ros/urdfdom_headers/archive/1.0.0.tar.gz"
   sha256 "f341e9956d53dc7e713c577eb9a8a7ee4139c8b6f529ce0a501270a851673001"
+  head "https://github.com/ros/urdfdom_headers", :branch => "master"
 
   depends_on "cmake" => :build
 
